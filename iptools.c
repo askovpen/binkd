f05c54b2e4507c1837b2a2a5f2d9701197daf399
gul <gul>
2003-03-01 18:46:05 +0000

    Use HAVE_SYS_IOCTL_H macro

--- iptools.c
+++ iptools.c
@@ -12,9 +12,12 @@
  */
 
 /*
- * $Id: iptools.c,v 2.3 2003/02/28 08:53:38 gul Exp $
+ * $Id: iptools.c,v 2.4 2003/03/01 18:46:05 gul Exp $
  *
  * $Log: iptools.c,v $
+ * Revision 2.4  2003/03/01 18:46:05  gul
+ * Use HAVE_SYS_IOCTL_H macro
+ *
  * Revision 2.3  2003/02/28 08:53:38  gul
  * Fixed proxy usage
  *
@@ -43,7 +46,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-#if !defined(WIN32)
+#if defined(HAVE_SYS_IOCTL_H)
 #include <sys/ioctl.h>
 #endif
 
