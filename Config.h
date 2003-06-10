4c395cb399c536eb36285b9d81f8b67d9b132ed1
stas <stas>
2003-06-10 12:29:16 +0000

    Auto increase patchlevel

--- Config.h
+++ Config.h
@@ -12,9 +12,12 @@
  */
 
 /*
- * $Id: Config.h,v 2.16 2003/06/10 07:43:37 gul Exp $
+ * $Id: Config.h,v 2.17 2003/06/10 12:29:16 stas Exp $
  *
  * $Log: Config.h,v $
+ * Revision 2.17  2003/06/10 12:29:16  stas
+ * Auto increase patchlevel
+ *
  * Revision 2.16  2003/06/10 07:43:37  gul
  * Auto increase patchlevel
  *
@@ -103,7 +106,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-5"
+#define MYVER "0.9.6a-6"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
