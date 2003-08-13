44fe17df31d31acee78f5388ad1b61c419d3580b
val <val>
2003-08-13 08:02:53 +0000

    Auto increase patchlevel, set 0.9.6a-87 define DELAY_ADR ifdef WITH_PERL (todo: provide more flexible logic)

--- Config.h
+++ Config.h
@@ -12,9 +12,13 @@
  */
 
 /*
- * $Id: Config.h,v 2.98 2003/08/12 09:35:50 gul Exp $
+ * $Id: Config.h,v 2.99 2003/08/13 08:02:53 val Exp $
  *
  * $Log: Config.h,v $
+ * Revision 2.99  2003/08/13 08:02:53  val
+ * Auto increase patchlevel, set 0.9.6a-87
+ * define DELAY_ADR ifdef WITH_PERL (todo: provide more flexible logic)
+ *
  * Revision 2.98  2003/08/12 09:35:50  gul
  * Auto increase patchlevel, set 0.9.6a-86
  * Cosmetics
@@ -427,7 +431,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-86"
+#define MYVER "0.9.6a-87"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
