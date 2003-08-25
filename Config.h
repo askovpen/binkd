fa63f4184ed5cf81b23c4636a0c51fda0743d294
gul <gul>
2003-08-25 18:25:36 +0000

    Auto increase patchlevel, set 0.9.6a-137 Remove partial if received part more then total size

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.151 2003/08/25 14:09:06 gul Exp $
+ * $Id: Config.h,v 2.152 2003/08/25 18:25:36 gul Exp $
  */
 
 #ifndef _Config_h
@@ -48,7 +48,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-136"
+#define MYVER "0.9.6a-137"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
