a63595ca601b5844c429fc04dfa39d599d7cd57f
gul <gul>
2003-08-26 18:18:53 +0000

    Auto increase patchlevel, set 0.9.6a-145 Process "-?" commandline switch as "-h"

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.160 2003/08/26 17:01:28 gul Exp $
+ * $Id: Config.h,v 2.161 2003/08/26 18:18:53 gul Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-144"
+#define MYVER "0.9.6a-145"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
