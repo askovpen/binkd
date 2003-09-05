c03715b326e9fa09434511ca423ea5cf933d474b
val <val>
2003-09-05 06:44:07 +0000

    Auto increase patchlevel, set 0.9.6a-154 Argus-style freq's (M_NUL FREQ) support, not tested yet

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.169 2003/08/30 16:38:56 gul Exp $
+ * $Id: Config.h,v 2.170 2003/09/05 06:44:07 val Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-153"
+#define MYVER "0.9.6a-154"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
