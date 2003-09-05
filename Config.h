e0809771f4398027a02335f4b4f5dac9c3269b1c
gul <gul>
2003-09-05 08:15:56 +0000

    Auto increase patchlevel, set 0.9.6a-156 Make DEBUG-version single-thread

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.171 2003/09/05 06:49:08 val Exp $
+ * $Id: Config.h,v 2.172 2003/09/05 08:15:56 gul Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-155"
+#define MYVER "0.9.6a-156"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
