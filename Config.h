b11947b0688496a1b80e4b9a9199ff51534c6ab6
stream <stream>
2003-09-08 17:05:44 +0000

    Auto increase patchlevel, set 0.9.6a-168 Big memory leak in evt_queue()

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.183 2003/09/08 16:39:41 stream Exp $
+ * $Id: Config.h,v 2.184 2003/09/08 17:05:44 stream Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-167"
+#define MYVER "0.9.6a-168"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
