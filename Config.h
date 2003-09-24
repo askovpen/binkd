c6eaf12533456b7cd76dc37c37de7cc9eb331a92
val <val>
2003-09-24 07:39:53 +0000

    Auto increase patchlevel, set 0.9.6a-191 fix warning

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.206 2003/09/24 07:32:52 val Exp $
+ * $Id: Config.h,v 2.207 2003/09/24 07:39:53 val Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-190"
+#define MYVER "0.9.6a-191"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
