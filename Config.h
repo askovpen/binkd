80e4435ae6a87963536e3015ab3816e3181cc552
val <val>
2003-10-03 13:30:31 +0000

    Auto increase patchlevel, set 0.9.6a-196 fix for older bzlib2 error (uses FILE but doesn't include stdio.h)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.211 2003/09/25 09:01:30 gul Exp $
+ * $Id: Config.h,v 2.212 2003/10/03 13:30:31 val Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-195"
+#define MYVER "0.9.6a-196"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
