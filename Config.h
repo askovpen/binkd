1a3d8108235730fd7baca04faa2ae17722189afa
stas <stas>
2003-10-06 16:54:53 +0000

    Auto increase patchlevel, set 1.0a-211 (Prevent warnings.) Prepare to implement full service control

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.227 2003/10/06 16:50:24 stas Exp $
+ * $Id: Config.h,v 2.228 2003/10/06 16:54:53 stas Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "1.0a-210"
+#define MYVER "1.0a-211"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
