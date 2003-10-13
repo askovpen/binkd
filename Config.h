ed3ec4350ddb5c84a8adfa157898bd3eb7fe2e8a
stas <stas>
2003-10-13 08:48:42 +0000

    Auto increase patchlevel, set 1.0a-237 Implement true NT service stop sequence

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.253 2003/10/13 08:48:13 stas Exp $
+ * $Id: Config.h,v 2.254 2003/10/13 08:48:42 stas Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "1.0a-236"
+#define MYVER "1.0a-237"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
