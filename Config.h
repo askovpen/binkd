d2eb4efeb42089b6991627336fab9527fafcabdd
stas <stas>
2003-10-18 18:50:51 +0000

    Auto increase patchlevel, set 1.0a-248 Move to new 'tray.c' file several functions when is related with 'minimize to tray' feature

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.264 2003/10/18 17:02:32 stas Exp $
+ * $Id: Config.h,v 2.265 2003/10/18 18:50:51 stas Exp $
  */
 
 #ifndef _Config_h
@@ -52,7 +52,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "1.0a-247"
+#define MYVER "1.0a-248"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
