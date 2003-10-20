bf93add06fa8c1820e5e9c8d812535e477af55c4
gul <gul>
2003-10-20 18:04:50 +0000

    Auto increase patchlevel, set 1.0a-261
    Previous patch break OS/2 compilation. Fixed.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.279 2003/10/20 17:57:16 gul Exp $
+ * $Id: Config.h,v 2.280 2003/10/20 18:04:50 gul Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 260
+#define PATCHLEVEL 261
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
