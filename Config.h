b49d8a0042f0359300b0d0b3472bf1d92a4b43c1
stas <stas>
2003-10-22 14:24:55 +0000

    Auto increase patchlevel, set 1.0a-267
    Remove obsolete defines

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.286 2003/10/22 14:24:51 stas Exp $
+ * $Id: Config.h,v 2.287 2003/10/22 14:24:55 stas Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 266
+#define PATCHLEVEL 267
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
