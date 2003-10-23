7ebff276fe85a5a93fc64c44355236d950198feb
gul <gul>
2003-10-23 21:38:51 +0000

    Remove C++ style comments

--- nt/breaksig.c
+++ nt/breaksig.c
@@ -20,10 +20,13 @@
 /*--------------------------------------------------------------------*/
 
 /*
- * $Id: breaksig.c,v 2.21 2003/10/17 04:20:29 hbrew Exp $
+ * $Id: breaksig.c,v 2.22 2003/10/23 21:38:52 gul Exp $
  *
  * Revision history:
  * $Log: breaksig.c,v $
+ * Revision 2.22  2003/10/23 21:38:52  gul
+ * Remove C++ style comments
+ *
  * Revision 2.21  2003/10/17 04:20:29  hbrew
  * Fix binkd9x atexit()
  *
@@ -111,10 +114,6 @@
 /*                        Local include files                         */
 /*--------------------------------------------------------------------*/
 
-//#include "..\sys.h"
-//#include "..\iphdr.h"
-//#include "..\bsy.h"
-//#include "..\binlog.h"
 #include "../readcfg.h"
 #include "../common.h"
 #include "../tools.h"
