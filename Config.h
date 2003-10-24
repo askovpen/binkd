e37a4534054670ad704326a0ce527ec3c41fdc96
stas <stas>
2003-10-24 18:18:44 +0000

    Auto increase patchlevel, set 1.0a-277 use macro instead number; remove unused comparision

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.296 2003/10/24 14:19:32 val Exp $
+ * $Id: Config.h,v 2.297 2003/10/24 18:18:44 stas Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 276
+#define PATCHLEVEL 277
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
