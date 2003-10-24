63fc047e6e7daf5cdef0772b300ec557aa4e4000
val <val>
2003-10-24 06:38:44 +0000

    Auto increase patchlevel, set 1.0a-273
    fix warning

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.292 2003/10/23 21:39:23 gul Exp $
+ * $Id: Config.h,v 2.293 2003/10/24 06:38:44 val Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 272
+#define PATCHLEVEL 273
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
