f9ad5695fcf2639ca35d5b939a5156f28298adad
gul <gul>
2003-10-27 23:23:55 +0000

    Auto increase patchlevel, set 1.0a-281 Fix static ZLIB/BZLIB linking

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.300 2003/10/27 23:22:57 gul Exp $
+ * $Id: Config.h,v 2.301 2003/10/27 23:23:55 gul Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 280
+#define PATCHLEVEL 281
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
