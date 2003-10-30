e9876f3bd72a157e99abda833a939d8d98617ab7
gul <gul>
2003-10-30 11:11:34 +0000

    Auto increase patchlevel, set 1.0a-292 Drop incoming session if secure remote AKA is busy

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.312 2003/10/30 10:57:48 gul Exp $
+ * $Id: Config.h,v 2.313 2003/10/30 11:11:34 gul Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 291
+#define PATCHLEVEL 292
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
