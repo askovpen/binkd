f1151caea68f1e22801847160c34e6c6306fcc28
gul <gul>
2003-10-30 10:57:48 +0000

    Auto increase patchlevel, set 1.0a-291 Change inb_done arguments, optimize a bit

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.311 2003/10/30 10:37:37 gul Exp $
+ * $Id: Config.h,v 2.312 2003/10/30 10:57:48 gul Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 290
+#define PATCHLEVEL 291
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
