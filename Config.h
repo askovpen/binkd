5566fed4699c8fb4e7165a0aa8996b8492347dd0
gul <gul>
2003-12-15 00:03:58 +0000

    Auto increase patchlevel, set 1.0a-309 NZ option for M_GET command - request uncompressed file

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.330 2003/12/10 11:12:15 gul Exp $
+ * $Id: Config.h,v 2.331 2003/12/15 00:03:58 gul Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 308
+#define PATCHLEVEL 309
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
