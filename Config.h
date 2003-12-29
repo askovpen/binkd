96c7fa2afbb9e9d2b36984f728a5d9861a089f9d
gul <gul>
2003-12-29 10:49:42 +0000

    Auto increase patchlevel, set 1.0a-317 Comments in config now starts by "#" only if prev char is space

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.339 2003/12/28 10:23:28 gul Exp $
+ * $Id: Config.h,v 2.340 2003/12/29 10:49:42 gul Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 316
+#define PATCHLEVEL 317
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
