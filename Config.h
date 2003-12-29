262697c5ab9b3b2cfd5ba00072a64d24952cffc7
val <val>
2003-12-29 10:55:18 +0000

    Auto increase patchlevel, set 1.0a-318 fix to pmatch(): symbol after '*' was case-sensitive in any case

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.340 2003/12/29 10:49:42 gul Exp $
+ * $Id: Config.h,v 2.341 2003/12/29 10:55:18 val Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 317
+#define PATCHLEVEL 318
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
