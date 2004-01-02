087a96a93780cb83dcc1d9e1fbd6e2ad21d660d7
stas <stas>
2004-01-02 21:20:19 +0000

    Auto increase patchlevel, set 1.0a-329 GetMainWindow(): function retrieves the window handle used by the main window of application

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.352 2004/01/02 20:58:59 stas Exp $
+ * $Id: Config.h,v 2.353 2004/01/02 21:20:19 stas Exp $
  */
 
 #ifndef _Config_h
@@ -58,7 +58,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 328
+#define PATCHLEVEL 329
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
