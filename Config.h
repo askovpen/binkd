667d73a10b7f546931be0a312cc2c1048dac01f3
stas <stas>
2004-01-02 15:32:02 +0000

    Auto increase patchlevel, set 1.0a-323 Fix the minfree token usage and fix getfree() on Win >w95

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.345 2004/01/02 15:31:33 stas Exp $
+ * $Id: Config.h,v 2.346 2004/01/02 15:32:02 stas Exp $
  */
 
 #ifndef _Config_h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 322
+#define PATCHLEVEL 323
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
