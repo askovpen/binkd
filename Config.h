d9bdeda7b0ee179d546d33b1b5e349f9d78dce5d
stas <stas>
2004-01-03 15:39:24 +0000

    Auto increase patchlevel, set 1.0a-336 Implement service control option for Windows NT/2k/XP

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.360 2004/01/03 15:38:54 stas Exp $
+ * $Id: Config.h,v 2.361 2004/01/03 15:39:24 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 335
+#define PATCHLEVEL 336
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
