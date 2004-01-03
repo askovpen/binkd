289223717456f58cdea1a26d04e9dc7d7a3d8034
stas <stas>
2004-01-03 19:04:52 +0000

    Auto increase patchlevel, set 1.0a-343 New functions: public w32Init() and hidden w32exitfunc()

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.367 2004/01/03 19:04:22 stas Exp $
+ * $Id: Config.h,v 2.368 2004/01/03 19:04:52 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 342
+#define PATCHLEVEL 343
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
