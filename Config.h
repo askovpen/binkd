20e4f28c369f5f6b68da482ff8fc20da7507d603
stas <stas>
2004-01-03 15:46:12 +0000

    Auto increase patchlevel, set 1.0a-337 Fix: do not load icon into service control window at binkd service starts

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.361 2004/01/03 15:39:24 stas Exp $
+ * $Id: Config.h,v 2.362 2004/01/03 15:46:12 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 336
+#define PATCHLEVEL 337
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
