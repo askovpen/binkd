5c29d087aa6bc08e61bc37239f5e054f669622ee
stas <stas>
2004-01-03 12:18:16 +0000

    Auto increase patchlevel, set 1.0a-332 Implement full icon support (winNT/2k/XP)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.356 2004/01/03 12:17:47 stas Exp $
+ * $Id: Config.h,v 2.357 2004/01/03 12:18:16 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 331
+#define PATCHLEVEL 332
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
