2d94143d9857559377c17600ee79ae44669c63cc
stas <stas>
2004-01-03 18:10:12 +0000

    Auto increase patchlevel, set 1.0a-338 Fix typo

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.362 2004/01/03 15:46:12 stas Exp $
+ * $Id: Config.h,v 2.363 2004/01/03 18:10:12 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 337
+#define PATCHLEVEL 338
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
