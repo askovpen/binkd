07863fb8bb8b89f10e6e416c23bf89c5c2c23c13
stas <stas>
2004-01-04 15:19:39 +0000

    Auto increase patchlevel, set 1.0a-346 Use Service Display Name to display (log, windows title)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.370 2004/01/03 19:33:44 stas Exp $
+ * $Id: Config.h,v 2.371 2004/01/04 15:19:39 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 345
+#define PATCHLEVEL 346
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
