7f98fa95439cc7f39610d6e44c416c473e8a83a7
gul <gul>
2004-01-07 12:09:47 +0000

    Auto increase patchlevel, set 1.0a-353 New function free_nodes()

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.377 2004/01/07 12:07:50 gul Exp $
+ * $Id: Config.h,v 2.378 2004/01/07 12:09:47 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 352
+#define PATCHLEVEL 353
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
