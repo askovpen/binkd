2614f0fed5a7052867ac5a023222e6c08555eb00
val <val>
2004-01-08 13:28:22 +0000

    Auto increase patchlevel, set 1.0a-365 * extend struct dirent for dos and win32 in order to get file attribute * ignore hidden files in boxes for dos/win32/os2 * if we can differ files from directories w/o stat(), don't call stat()   when scanning boxes (unix: freebsd) * if we can't unlink file, don't send it again in the same session * for dos/win32/os2 try to clear read/only attribute if can't unlink file

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.389 2004/01/08 13:27:52 val Exp $
+ * $Id: Config.h,v 2.390 2004/01/08 13:28:22 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 364
+#define PATCHLEVEL 365
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
