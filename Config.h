3d642040684cd9a59613fbdadaff41491d863b4f
gul <gul>
2004-01-08 11:36:09 +0000

    Auto increase patchlevel, set 1.0a-358 Fix typo in previous patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.382 2004/01/07 21:42:26 stas Exp $
+ * $Id: Config.h,v 2.383 2004/01/08 11:36:09 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 357
+#define PATCHLEVEL 358
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
