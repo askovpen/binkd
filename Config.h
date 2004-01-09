319d0fb72238008c65e03e39cc7915f7850f9f49
gul <gul>
2004-01-09 10:55:35 +0000

    Auto increase patchlevel, set 1.0a-366 Avoid gcc 3.x warning

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.390 2004/01/08 13:28:22 val Exp $
+ * $Id: Config.h,v 2.391 2004/01/09 10:55:35 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 365
+#define PATCHLEVEL 366
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
