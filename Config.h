78275b071cbf4af6c072bd399aa4fca50d0d7379
gul <gul>
2004-01-23 18:09:34 +0000

    Auto increase patchlevel, set 1.0a-370 Fixed erroneous "Unknown option ... ignored" message

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.394 2004/01/19 08:51:40 hbrew Exp $
+ * $Id: Config.h,v 2.395 2004/01/23 18:09:34 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 369
+#define PATCHLEVEL 370
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
