1f82e9120ff42d65f90ae57eda2794166a12c05f
hbrew <hbrew>
2004-02-07 14:06:09 +0000

    Auto increase patchlevel, set 1.0a-372 Macros: RTLDLL-->RTLSTATIC, BINKDW9X-->BINKD9X

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.397 2004/02/07 14:06:06 hbrew Exp $
+ * $Id: Config.h,v 2.398 2004/02/07 14:06:09 hbrew Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 371
+#define PATCHLEVEL 372
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
