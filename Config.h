3ebcbdd28a0ec525e3fd54d6156d7abd4dc0323c
gul <gul>
2004-02-29 08:51:54 +0000

    Auto increase patchlevel, set 1.0a-376 Bugfix in print_node_info (patch from Victor Levenets).

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.401 2004/02/29 08:51:17 gul Exp $
+ * $Id: Config.h,v 2.402 2004/02/29 08:51:54 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 375
+#define PATCHLEVEL 376
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
