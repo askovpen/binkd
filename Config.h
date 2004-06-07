9156dab6426a1e5496edb2a7cae4a610819d7ebb
gul <gul>
2004-06-07 10:47:16 +0000

    Auto increase patchlevel, set 1.0a-378 Touch flag if its already exists

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.403 2004/03/29 16:26:21 stas Exp $
+ * $Id: Config.h,v 2.404 2004/06/07 10:47:16 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 377
+#define PATCHLEVEL 378
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
