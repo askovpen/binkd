3278f55a3edd1c477afc3721b2e389a0939f0502
stas <stas>
2004-07-12 08:21:58 +0000

    Auto increase patchlevel, set 1.0a-379 Fix the file name case. Bugreport from Andrey Slusar 2:467/126

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.404 2004/06/07 10:47:16 gul Exp $
+ * $Id: Config.h,v 2.405 2004/07/12 08:21:58 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 378
+#define PATCHLEVEL 379
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
