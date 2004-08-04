6fe507547a74912bdfc765d2860c0a754913a30c
gul <gul>
2004-08-04 11:33:14 +0000

    Auto increase patchlevel, set 1.0a-384 Attemp to support large files (>4G)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.409 2004/08/04 06:41:11 gul Exp $
+ * $Id: Config.h,v 2.410 2004/08/04 11:33:14 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 383
+#define PATCHLEVEL 384
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
