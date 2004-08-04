c0ea6ffa4940539ec70a7cfd8e1c9ec35aa2c7ff
gul <gul>
2004-08-04 13:15:45 +0000

    Auto increase patchlevel, set 1.0a-386 Define u16 and u32 types more clean

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.411 2004/08/04 13:07:05 gul Exp $
+ * $Id: Config.h,v 2.412 2004/08/04 13:15:45 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 385
+#define PATCHLEVEL 386
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
