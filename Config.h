9e40cea010e4305a9998d3960a2f05902dcedd9b
gul <gul>
2004-08-04 13:07:05 +0000

    Auto increase patchlevel, set 1.0a-385 Binlogs writes correctly now independent on bytes order and alignment

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.410 2004/08/04 11:33:14 gul Exp $
+ * $Id: Config.h,v 2.411 2004/08/04 13:07:05 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 384
+#define PATCHLEVEL 385
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
