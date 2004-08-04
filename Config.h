3a1571d2e1d6bd2b21e8c344cd7d96f3454b2546
gul <gul>
2004-08-04 19:52:18 +0000

    Auto increase patchlevel, set 1.0a-388 Change SIGCHLD handling, make signal handler more clean, prevent occasional hanging (mutex deadlock) under linux kernel 2.6.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.413 2004/08/04 13:29:06 gul Exp $
+ * $Id: Config.h,v 2.414 2004/08/04 19:52:18 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 387
+#define PATCHLEVEL 388
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
