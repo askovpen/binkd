7d4e02af1d0811024e260009b7adb779de804f7d
hbrew <hbrew>
2004-08-11 18:31:23 +0000

    Auto increase patchlevel, set 1.0a-390 zlibdl.h: include <windows.h> for compatibility with zlib 1.2.1

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.415 2004/08/06 07:05:31 gul Exp $
+ * $Id: Config.h,v 2.416 2004/08/11 18:31:23 hbrew Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 389
+#define PATCHLEVEL 390
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
