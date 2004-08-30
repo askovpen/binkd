6c58729b50e28fc5746c879bcdd52aa03c3222e4
val <val>
2004-08-30 08:05:40 +0000

    Auto increase patchlevel, set 1.0a-393 bandwidth limiting code [experimental]

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.418 2004/08/24 11:31:44 gul Exp $
+ * $Id: Config.h,v 2.419 2004/08/30 08:05:40 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 392
+#define PATCHLEVEL 393
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
