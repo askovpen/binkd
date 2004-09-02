0cf67eb0f08c3c952af0bb2af0ac46527913e74c
val <val>
2004-09-02 09:32:00 +0000

    Auto increase patchlevel, set 1.0a-396 release CPU when limiting bandwidth on Win32

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.421 2004/09/02 09:01:00 val Exp $
+ * $Id: Config.h,v 2.422 2004/09/02 09:32:00 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 395
+#define PATCHLEVEL 396
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
