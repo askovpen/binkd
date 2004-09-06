c251e36d4a8757eba41007d2deee63e9bed0c9d3
val <val>
2004-09-06 10:47:14 +0000

    Auto increase patchlevel, set 1.0a-397 bandwidth limiting code advancements, `listed' session state fix

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.422 2004/09/02 09:32:00 val Exp $
+ * $Id: Config.h,v 2.423 2004/09/06 10:47:14 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 396
+#define PATCHLEVEL 397
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
