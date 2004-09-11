b33ca8f7ca0051121f89e248a923b7c8fa8f5fa8
gul <gul>
2004-09-11 12:34:18 +0000

    Auto increase patchlevel, set 1.0a-399 Check for zonenumber syntax in the "domain" keyword - it should be decimal integer. Zone "3c3" is now error, but not 3.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.424 2004/09/06 10:50:15 val Exp $
+ * $Id: Config.h,v 2.425 2004/09/11 12:34:18 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 398
+#define PATCHLEVEL 399
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
