7822b2f9867bef427eb081eb8468554a58ebe625
gul <gul>
2004-09-11 14:37:17 +0000

    Auto increase patchlevel, set 1.0a-401 Fix erroneously applied previous patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.426 2004/09/11 13:57:20 gul Exp $
+ * $Id: Config.h,v 2.427 2004/09/11 14:37:17 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 400
+#define PATCHLEVEL 401
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
