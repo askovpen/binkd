f742e27d2bd67f673be8bd8ef01b4b7ff31d0b0b
gul <gul>
2004-09-11 13:57:20 +0000

    Auto increase patchlevel, set 1.0a-400 Remove duplicate line

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.425 2004/09/11 12:34:18 gul Exp $
+ * $Id: Config.h,v 2.426 2004/09/11 13:57:20 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 399
+#define PATCHLEVEL 400
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
