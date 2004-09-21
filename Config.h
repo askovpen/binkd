9e94e00a70d57fdc3e974e27377dae655406e2bc
val <val>
2004-09-21 08:27:55 +0000

    Auto increase patchlevel, set 1.0a-402 distinguish nodes, listed in binkd config and passwords file - overwrite defnode parameters (e.g. host) for the later (hope, it'll fix reported bug with not calling defnode)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.427 2004/09/11 14:37:17 gul Exp $
+ * $Id: Config.h,v 2.428 2004/09/21 08:27:55 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 401
+#define PATCHLEVEL 402
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
