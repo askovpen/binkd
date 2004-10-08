dc1a06975d2b6be3847dcc790c895bd1c40bdfd8
gul <gul>
2004-10-08 08:30:37 +0000

    Auto increase patchlevel, set 1.0a-405 binlog rev.2.11 bugfix

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.430 2004/10/01 09:55:22 gul Exp $
+ * $Id: Config.h,v 2.431 2004/10/08 08:30:37 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 404
+#define PATCHLEVEL 405
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
