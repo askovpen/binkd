fc4787877a6876132cf5c3f8b785e47ecf9d21cf
stas <stas>
2004-10-15 10:24:17 +0000

    Auto increase patchlevel, set 1.0a-406 binkd/win32/win95: Add "W32 API error" string into system error messages

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.431 2004/10/08 08:30:37 gul Exp $
+ * $Id: Config.h,v 2.432 2004/10/15 10:24:17 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 405
+#define PATCHLEVEL 406
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
