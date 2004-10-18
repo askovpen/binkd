fb4af8c201af07776914355d3536f8ad1681aa93
gul <gul>
2004-10-18 10:29:29 +0000

    Bugfix in MD_getChallenge(), thanks to Victor Levenets <aq@takas.lt>

--- md5b.c
+++ md5b.c
@@ -24,9 +24,12 @@ documentation and/or software.
  */
 
 /*
- * $Id: md5b.c,v 2.8 2003/10/29 21:08:39 gul Exp $
+ * $Id: md5b.c,v 2.9 2004/10/18 10:29:29 gul Exp $
  *
  * $Log: md5b.c,v $
+ * Revision 2.9  2004/10/18 10:29:29  gul
+ * Bugfix in MD_getChallenge(), thanks to Victor Levenets <aq@takas.lt>
+ *
  * Revision 2.8  2003/10/29 21:08:39  gul
  * Change include-files structure, relax dependences
  *
@@ -465,7 +468,7 @@ unsigned char *MD_getChallenge(char *str, STATE *rnd)
     for(i=1;i<(MD_CHALLENGE_LEN+1);i+=MD5_DIGEST_LEN) 
     {
       int k=MD5_DIGEST_LEN;
-      if(k>=i+MD_CHALLENGE_LEN) k=MD_CHALLENGE_LEN+1-i;
+      if(k>MD_CHALLENGE_LEN-i) k=MD_CHALLENGE_LEN-i;
       getrand(res+i, k, rnd);
     }
   }
