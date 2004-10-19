bd2dc8adca14f35b5bb8778aa6fa742aa69152e9
gul <gul>
2004-10-19 17:09:11 +0000

    Auto increase patchlevel, set 1.0a-411 Remove duplicate addresses from config

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.436 2004/10/19 16:28:26 gul Exp $
+ * $Id: Config.h,v 2.437 2004/10/19 17:09:11 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 410
+#define PATCHLEVEL 411
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
