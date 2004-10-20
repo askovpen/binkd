0b0b93e5ed0f045ca5247c2d3b3193d492d1f0eb
gul <gul>
2004-10-20 17:31:06 +0000

    Auto increase patchlevel, set 1.0a-412 Fixed segfault on transmission compressed files when sizeof(off_t)!=sizeof(long) and loglevel>3.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.437 2004/10/19 17:09:11 gul Exp $
+ * $Id: Config.h,v 2.438 2004/10/20 17:31:06 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 411
+#define PATCHLEVEL 412
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
