53d38edcc18e2f9cbf179784376a07b1c11f2ca7
gul <gul>
2004-10-25 17:04:59 +0000

    Auto increase patchlevel, set 1.0a-413 Process passwords file after all, independent of its place in config. Use first password for node if several specified.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.438 2004/10/20 17:31:06 gul Exp $
+ * $Id: Config.h,v 2.439 2004/10/25 17:04:59 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 412
+#define PATCHLEVEL 413
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
