25270ebd978288a7c06b8efeb82de06aeb28733b
gul <gul>
2004-10-26 17:37:05 +0000

    Auto increase patchlevel, set 1.0a-415 Set time intervals in human-readable form (12h instead of 43200 etc.)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.440 2004/10/26 15:46:47 gul Exp $
+ * $Id: Config.h,v 2.441 2004/10/26 17:37:05 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 414
+#define PATCHLEVEL 415
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
