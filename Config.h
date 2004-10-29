73b2bcf499c8007bce0bdb98b06af0a9df8a827b
stas <stas>
2004-10-29 13:05:15 +0000

    Auto increase patchlevel, set 1.0a-417 Add diagnostic about free space great 4Tb (win32 only)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.442 2004/10/29 11:16:38 gul Exp $
+ * $Id: Config.h,v 2.443 2004/10/29 13:05:15 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 416
+#define PATCHLEVEL 417
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
