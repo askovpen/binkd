08af594b588f789a4c0c9ac1de67865027daaecd
gul <gul>
2004-11-01 13:05:44 +0000

    Auto increase patchlevel, set 1.0a-418 Bugfix in connect-timeout

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.443 2004/10/29 13:05:15 stas Exp $
+ * $Id: Config.h,v 2.444 2004/11/01 13:05:44 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 417
+#define PATCHLEVEL 418
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
