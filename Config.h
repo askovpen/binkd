149a91bc81528b282ed01559335198a462ad482c
stas <stas>
2004-11-02 13:23:32 +0000

    Auto increase patchlevel, set 1.0a-419 More fix getfree() (win32)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.444 2004/11/01 13:05:44 gul Exp $
+ * $Id: Config.h,v 2.445 2004/11/02 13:23:32 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 418
+#define PATCHLEVEL 419
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
