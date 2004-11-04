05d8ca8ae6ab0ad7b3f74e3954e8fd4b688aa41d
stas <stas>
2004-11-04 06:22:04 +0000

    Auto increase patchlevel, set 1.0a-421 BUGFIX: segfault if 1st address is 4D

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.446 2004/11/03 08:22:30 stas Exp $
+ * $Id: Config.h,v 2.447 2004/11/04 06:22:04 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 420
+#define PATCHLEVEL 421
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
