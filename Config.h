75d76f0bd77e6674b4f51a863c939fb4b2446f84
gul <gul>
2004-11-05 12:44:31 +0000

    Auto increase patchlevel, set 1.0a-426 Client manager did not reload config on change in fork versions (unix, os2-emx)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.451 2004/11/05 12:25:09 gul Exp $
+ * $Id: Config.h,v 2.452 2004/11/05 12:44:31 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 425
+#define PATCHLEVEL 426
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
