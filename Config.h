17a6c3194e0b6a6dfacb04854b360384dd847650
val <val>
2004-11-21 12:18:33 +0000

    Auto increase patchlevel, set 1.0a-436 bandwidth limiting code is now implemented for receiving too

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.461 2004/11/21 11:55:45 val Exp $
+ * $Id: Config.h,v 2.462 2004/11/21 12:18:33 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 435
+#define PATCHLEVEL 436
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
