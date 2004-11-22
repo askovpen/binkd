ed68b7e9cf11331ad85423b004fc92ef2321fcce
stream <stream>
2004-11-22 15:56:48 +0000

    Auto increase patchlevel, set 1.0a-438 Errors in config were not logged to file (log file name was set only when config was completely loaded, checked and accepted). Now log settings are changed immediately after each log-related directive (only for first-time config load)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.463 2004/11/22 15:51:21 stas Exp $
+ * $Id: Config.h,v 2.464 2004/11/22 15:56:48 stream Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 437
+#define PATCHLEVEL 438
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
