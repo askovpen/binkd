c709b0aadac08a08c2cf7c1a08e9f07b69e5e293
gul <gul>
2005-04-05 07:31:49 +0000

    Auto increase patchlevel, set 1.0a-442 Fixed bug in have_intmax_t detection

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.467 2005/03/30 17:35:33 stream Exp $
+ * $Id: Config.h,v 2.468 2005/04/05 07:31:49 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 441
+#define PATCHLEVEL 442
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
