6b9e7332b7ce7a6da3a1344568b3b3c9018d1c65
stream <stream>
2005-04-18 16:46:46 +0000

    Auto increase patchlevel, set 1.0a-443 Double free of pNodArray caused mysterious crashes on config reload

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.468 2005/04/05 07:31:49 gul Exp $
+ * $Id: Config.h,v 2.469 2005/04/18 16:46:46 stream Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 442
+#define PATCHLEVEL 443
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
