20f3f008c42e7337925a46f1c7d2506ffe188dcf
stas <stas>
2005-05-13 11:12:10 +0000

    Auto increase patchlevel, set 1.0a-444 update coment for struct _FTNQ

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.469 2005/04/18 16:46:46 stream Exp $
+ * $Id: Config.h,v 2.470 2005/05/13 11:12:10 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 443
+#define PATCHLEVEL 444
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
