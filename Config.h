8654384ca06c382b65518e851d9cc404abeb3aaa
stream <stream>
2005-06-06 16:43:44 +0000

    Auto increase patchlevel, set 1.0a-445 Updates for OS/2+Watcom+zlib compilation

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.470 2005/05/13 11:12:10 stas Exp $
+ * $Id: Config.h,v 2.471 2005/06/06 16:43:44 stream Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 444
+#define PATCHLEVEL 445
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
