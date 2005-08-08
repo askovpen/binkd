b0fdb5e192fa94b61d3bbf6e3f55b48adee7b9cd
val <val>
2005-08-08 10:12:14 +0000

    Auto increase patchlevel, set 1.0a-449 fix segfault when a remote doesn't provide an address but sends a file (bug reported by andrew clarke)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.474 2005/07/04 18:24:49 gul Exp $
+ * $Id: Config.h,v 2.475 2005/08/08 10:12:14 val Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 448
+#define PATCHLEVEL 449
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
