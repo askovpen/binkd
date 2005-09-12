0479c57121fec17e2789e78b30ed03d2b8e7260f
gul <gul>
2005-09-12 17:07:42 +0000

    Auto increase patchlevel, set 1.0a-450 Turn compression on if OPT EXTCMD GZIP received after login phase

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.475 2005/08/08 10:12:14 val Exp $
+ * $Id: Config.h,v 2.476 2005/09/12 17:07:42 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 449
+#define PATCHLEVEL 450
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
