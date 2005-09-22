7b24a2ca5cc1ad18236bf38db545182d538df197
gul <gul>
2005-09-22 12:11:24 +0000

    Auto increase patchlevel, set 1.0a-451 Dequote filenames for compare in M_GET processing

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.476 2005/09/12 17:07:42 gul Exp $
+ * $Id: Config.h,v 2.477 2005/09/22 12:11:24 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 450
+#define PATCHLEVEL 451
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
