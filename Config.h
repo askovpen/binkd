b7de00928bd066461c183fb03d80d975d42447a1
gul <gul>
2005-09-23 14:08:59 +0000

    Auto increase patchlevel, set 1.0a-455 Change sprintf() to snprintf() in https and ntlm code.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.480 2005/09/23 13:32:51 gul Exp $
+ * $Id: Config.h,v 2.481 2005/09/23 14:08:59 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 454
+#define PATCHLEVEL 455
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
