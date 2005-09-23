c2ff828960a9eec3cd1398ed1f48cf49c573bab0
gul <gul>
2005-09-23 13:04:20 +0000

    Auto increase patchlevel, set 1.0a-453 Fix warning

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.478 2005/09/23 12:24:38 gul Exp $
+ * $Id: Config.h,v 2.479 2005/09/23 13:04:20 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 452
+#define PATCHLEVEL 453
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
