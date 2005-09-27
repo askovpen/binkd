32817cd54fc7563ee63702c1e7eafb487de5ee6a
gul <gul>
2005-09-27 20:15:49 +0000

    Auto increase patchlevel, set 1.0a-458 Hopefully fixed compilation under windows

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.483 2005/09/26 19:01:12 gul Exp $
+ * $Id: Config.h,v 2.484 2005/09/27 20:15:49 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 457
+#define PATCHLEVEL 458
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
