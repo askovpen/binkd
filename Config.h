b86dbc187297d59e4bbf53b64db9656cb661b688
gul <gul>
2005-09-28 19:02:33 +0000

    Auto increase patchlevel, set 1.0a-461 Fixed unsigned int arithmetics in rate-limits

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.486 2005/09/28 07:18:54 gul Exp $
+ * $Id: Config.h,v 2.487 2005/09/28 19:02:33 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 460
+#define PATCHLEVEL 461
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
