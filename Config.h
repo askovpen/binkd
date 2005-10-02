4772883f9faf478818655091959faf301d444a93
gul <gul>
2005-10-02 21:47:41 +0000

    Auto increase patchlevel, set 1.0a-468 set_rlimit() perl hook

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.493 2005/10/02 20:48:47 gul Exp $
+ * $Id: Config.h,v 2.494 2005/10/02 21:47:41 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 467
+#define PATCHLEVEL 468
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
