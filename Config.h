e1920ccd87e6573f96f3337c9ccb1fc2225d1121
gul <gul>
2005-10-03 06:49:11 +0000

    Auto increase patchlevel, set 1.0a-469 Fixed typos in previous patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.494 2005/10/02 21:47:41 gul Exp $
+ * $Id: Config.h,v 2.495 2005/10/03 06:49:11 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 468
+#define PATCHLEVEL 469
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
