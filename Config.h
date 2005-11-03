8acf7aa47442111786fdc33e2d297675f761c0bc
stas <stas>
2005-11-03 13:15:57 +0000

    Auto increase patchlevel, set 1.0a-478 Option '-n' recognization

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.503 2005/11/03 11:42:42 stas Exp $
+ * $Id: Config.h,v 2.504 2005/11/03 13:15:57 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 477
+#define PATCHLEVEL 478
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
