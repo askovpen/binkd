8abc608684311391e4428ea0409aac147e4dc2e1
stas <stas>
2005-11-03 11:39:32 +0000

    Auto increase patchlevel, set 1.0a-476 Fix warning about signed-unsigned incompatibility

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.501 2005/10/28 07:13:51 stas Exp $
+ * $Id: Config.h,v 2.502 2005/11/03 11:39:32 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 475
+#define PATCHLEVEL 476
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
