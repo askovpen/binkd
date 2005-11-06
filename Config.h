f9d5c7686a775823506ad19350c7189813b38385
stas <stas>
2005-11-06 18:03:53 +0000

    Auto increase patchlevel, set 1.0a-479 snprintf() now knowns about long long arguments

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.504 2005/11/03 13:15:57 stas Exp $
+ * $Id: Config.h,v 2.505 2005/11/06 18:03:53 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 478
+#define PATCHLEVEL 479
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
