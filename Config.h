4e969614068c04cf9ae13b302851ec685b36291d
stas <stas>
2005-11-07 17:54:09 +0000

    Auto increase patchlevel, set 1.0a-481 rtrim

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.506 2005/11/06 18:43:10 stas Exp $
+ * $Id: Config.h,v 2.507 2005/11/07 17:54:09 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 480
+#define PATCHLEVEL 481
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
