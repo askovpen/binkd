0577b6c7a6ce207ced1b167dfc20406350f6b637
stas <stas>
2005-11-07 18:25:18 +0000

    Auto increase patchlevel, set 1.0a-484 fix signed/unsigned comparition

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.509 2005/11/07 17:59:38 stas Exp $
+ * $Id: Config.h,v 2.510 2005/11/07 18:25:18 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 483
+#define PATCHLEVEL 484
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
