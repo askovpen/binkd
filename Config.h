c1057ed6f47aef8ac51fc3d817e67e0da68b1407
gul <gul>
2005-11-08 13:58:37 +0000

    Auto increase patchlevel, set 1.0a-486 Fixed logic in condition in previous patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.511 2005/11/08 13:35:29 gul Exp $
+ * $Id: Config.h,v 2.512 2005/11/08 13:58:37 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 485
+#define PATCHLEVEL 486
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
