8a1181c517010e0f5f66d775bc9371ae6866b8ad
stas <stas>
2005-11-09 14:14:05 +0000

    Auto increase patchlevel, set 1.0a-487 Remove empty old .hr files

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.512 2005/11/08 13:58:37 gul Exp $
+ * $Id: Config.h,v 2.513 2005/11/09 14:14:05 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 486
+#define PATCHLEVEL 487
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
