9e0391ff93bf0a4a2a6ec6d088a3d352f7b1f487
stas <stas>
2005-11-09 14:16:01 +0000

    Auto increase patchlevel, set 1.0a-488 Increase loglevel for low-level file operation 'remove file' to dont double log lines

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.513 2005/11/09 14:14:05 stas Exp $
+ * $Id: Config.h,v 2.514 2005/11/09 14:16:01 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 487
+#define PATCHLEVEL 488
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
