ba2b10ed1c16f2f85ad50ecfb09134fdd4ca84c4
gul <gul>
2006-08-09 07:09:17 +0000

    Auto increase patchlevel, set 1.0a-495 cosmetic fix

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.520 2006/07/24 21:01:40 gul Exp $
+ * $Id: Config.h,v 2.521 2006/08/09 07:09:17 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 494
+#define PATCHLEVEL 495
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
