bbf7aa9802481e1054c692f28a4f24cb32791262
stas <stas>
2006-12-19 14:11:11 +0000

    Auto increase patchlevel, set 1.0a-499 Minimize required access rights to operate with Windows NT/2000/XP/2003 services

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.524 2006/10/25 09:49:08 stas Exp $
+ * $Id: Config.h,v 2.525 2006/12/19 14:11:11 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 498
+#define PATCHLEVEL 499
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
