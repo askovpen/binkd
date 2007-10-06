e2d13d23020516697e75d4b0bcc9e2df8521885b
gul <gul>
2007-10-06 09:35:17 +0000

    Auto increase patchlevel, set 1.0a-505 Retranslate SIGHUP from servermgr to clientmgr

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.530 2007/10/04 17:30:32 gul Exp $
+ * $Id: Config.h,v 2.531 2007/10/06 09:35:17 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 504
+#define PATCHLEVEL 505
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
