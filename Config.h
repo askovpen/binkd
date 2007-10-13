0553ec9f6f64cfe758004046fcaabd999f0b263c
gul <gul>
2007-10-13 05:35:19 +0000

    Auto increase patchlevel, set 1.0a-507 play around checkcfg()

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.532 2007/10/06 10:20:09 gul Exp $
+ * $Id: Config.h,v 2.533 2007/10/13 05:35:19 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 506
+#define PATCHLEVEL 507
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
