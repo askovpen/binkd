d0b3274d6fe75497d53329f5bc4e18ddd1022f9b
gul <gul>
2007-10-30 07:43:12 +0000

    Auto increase patchlevel, set 1.0a-509 Removed copy/pasted code from prev patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.534 2007/10/30 07:33:27 gul Exp $
+ * $Id: Config.h,v 2.535 2007/10/30 07:43:12 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 508
+#define PATCHLEVEL 509
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
