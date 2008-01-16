4d8b45ca3f71d0db729b52debc9b362d31b93920
gul <gul>
2008-01-16 10:05:45 +0000

    Auto increase patchlevel, set 1.0a-517 Fix for previous patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.542 2008/01/15 23:08:21 stas Exp $
+ * $Id: Config.h,v 2.543 2008/01/16 10:05:45 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 516
+#define PATCHLEVEL 517
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
