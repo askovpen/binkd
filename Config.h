75715068591fb82d18714f40f43b73447a823261
gul <gul>
2008-02-25 10:38:16 +0000

    Auto increase patchlevel, set 1.0a-518 Fixed incorrect byte counters in log message about compressed files

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.543 2008/01/16 10:05:45 gul Exp $
+ * $Id: Config.h,v 2.544 2008/02/25 10:38:16 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 517
+#define PATCHLEVEL 518
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
