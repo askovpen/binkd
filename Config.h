b4e26048cfd3ac48c92deb79fc82f163cd608924
gul <gul>
2008-12-09 09:04:13 +0000

    Auto increase patchlevel, set 1.0a-522 Use binkp from /etc/services if exists as iport/oport by default

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.547 2008/11/25 09:35:31 gul Exp $
+ * $Id: Config.h,v 2.548 2008/12/09 09:04:13 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 521
+#define PATCHLEVEL 522
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
