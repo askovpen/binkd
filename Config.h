090ce03933d57ed1b4e46006c81dd26683970714
gul <gul>
2009-01-31 16:58:05 +0000

    Auto increase patchlevel, set 1.0a-523 Update year in copyright line

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.548 2008/12/09 09:04:13 gul Exp $
+ * $Id: Config.h,v 2.549 2009/01/31 16:58:05 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 522
+#define PATCHLEVEL 523
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
