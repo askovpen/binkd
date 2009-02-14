f97f4ebb4dc0d9a599c848191f0a518d0a4ad6cf
gul <gul>
2009-02-14 13:14:46 +0000

    Auto increase patchlevel, set 1.0a-525 Bugfix: segfault on crafted input sequences, possible remote DoS for multithread versions (win32 and OS/2). Thanks to Dennis Yurichev.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.550 2009/02/04 20:13:50 gul Exp $
+ * $Id: Config.h,v 2.551 2009/02/14 13:14:46 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 524
+#define PATCHLEVEL 525
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
