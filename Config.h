c846967e8b09435a8e5187141c75dea51106c140
gul <gul>
2009-05-26 13:04:39 +0000

    Auto increase patchlevel, set 1.0a-528 New perl hooks: need_reload() - is it needed to reload config config_loaded() - after successful reading config

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.553 2009/05/25 17:02:09 gul Exp $
+ * $Id: Config.h,v 2.554 2009/05/26 13:04:39 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 527
+#define PATCHLEVEL 528
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
