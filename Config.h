d5eeeb6a19962ffe1150b5c1823cc045bd242725
gul <gul>
2009-05-27 09:49:54 +0000

    Auto increase patchlevel, set 1.0a-530 Free perl-vars on unloading config

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.555 2009/05/27 09:33:56 gul Exp $
+ * $Id: Config.h,v 2.556 2009/05/27 09:49:54 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 529
+#define PATCHLEVEL 530
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
