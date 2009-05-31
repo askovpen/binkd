61ab0afa98275ebb0b3a37685c92efefd08e8a3d
gul <gul>
2009-05-31 07:16:50 +0000

    Auto increase patchlevel, set 1.0a-531 Warning: many changes, may be unstable. Perl interpreter is now part of config and rerun on config reload. Perl 5.10 compatibility. Changes in outbound queue managing and sorting.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.556 2009/05/27 09:49:54 gul Exp $
+ * $Id: Config.h,v 2.557 2009/05/31 07:16:50 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 530
+#define PATCHLEVEL 531
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
