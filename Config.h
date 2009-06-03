d78e20ca8da9780ca3cbc231f086907ee2d68613
gul <gul>
2009-06-03 07:50:57 +0000

    Auto increase patchlevel, set 1.0a-533 $_ mistakenly set as read-only in need_reload() perl hook, fixed.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.558 2009/06/02 17:10:12 gul Exp $
+ * $Id: Config.h,v 2.559 2009/06/03 07:50:57 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 532
+#define PATCHLEVEL 533
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
