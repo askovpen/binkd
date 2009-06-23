fe71caf27c8be516d47d0526720480197c70a9c6
gul <gul>
2009-06-23 06:20:39 +0000

    Auto increase patchlevel, set 1.0a-539 Compilation error on win32, fixed

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.564 2009/06/16 19:24:35 gul Exp $
+ * $Id: Config.h,v 2.565 2009/06/23 06:20:39 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 538
+#define PATCHLEVEL 539
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
