6d579d816f26b91a01e464e73679cd30996b43d0
gul <gul>
2009-06-23 10:53:26 +0000

    Auto increase patchlevel, set 1.0a-540 Perl <5.10 win32 PERLDL compatibility (no sv_free2 call)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.565 2009/06/23 06:20:39 gul Exp $
+ * $Id: Config.h,v 2.566 2009/06/23 10:53:26 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 539
+#define PATCHLEVEL 540
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
