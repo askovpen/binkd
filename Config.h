5e15edd5296c6bfb6edc29e8e9eea915eec5607e
gul <gul>
2009-11-22 07:52:55 +0000

    Auto increase patchlevel, set 1.0a-541 Send M_ERR and increase undialable on error rename received file

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.566 2009/06/23 10:53:26 gul Exp $
+ * $Id: Config.h,v 2.567 2009/11/22 07:52:55 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 540
+#define PATCHLEVEL 541
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
