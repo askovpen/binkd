f493f96f03cdc6c2aad6a47cc26c1d28c860d5cc
gul <gul>
2010-03-30 06:13:03 +0000

    Auto increase patchlevel, set 1.0a-545 Do not chdir to "/" on daemonize for use relative pathes on reload config

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.570 2010/03/30 06:11:34 gul Exp $
+ * $Id: Config.h,v 2.571 2010/03/30 06:13:03 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 544
+#define PATCHLEVEL 545
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
