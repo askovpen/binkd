75fe0c131019b1a94176218b79b55746bb9ce97e
gul <gul>
2010-05-22 08:11:33 +0000

    Auto increase patchlevel, set 1.0a-546 Call after_session() hook after removing bsy

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.571 2010/03/30 06:13:03 gul Exp $
+ * $Id: Config.h,v 2.572 2010/05/22 08:11:33 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 545
+#define PATCHLEVEL 546
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
