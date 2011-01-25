a5558ec145e1036f30ed1369b57239c1ca01e2bb
stas <stas>
2011-01-25 15:33:52 +0000

    Auto increase patchlevel, set 1.0a-556 Prevent segfault if length of passwords exceed 40 chars (addition for previous patch

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.581 2011/01/24 10:44:54 gul Exp $
+ * $Id: Config.h,v 2.582 2011/01/25 15:33:52 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 555
+#define PATCHLEVEL 556
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
