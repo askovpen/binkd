70e57bad26958a014f0f88891c13b4eda8c80650
gul <gul>
2011-02-19 06:08:30 +0000

    Auto increase patchlevel, set 1.0a-557 Yet another possible segfault on session start

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.582 2011/01/25 15:33:52 stas Exp $
+ * $Id: Config.h,v 2.583 2011/02/19 06:08:30 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 556
+#define PATCHLEVEL 557
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
