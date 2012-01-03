cf4d68765827aedebca35900dfd6e04993e670f4
green <green>
2012-01-03 17:26:05 +0000

    Auto increase patchlevel, set 1.0a-564 Implemented IPv6 support - replace (almost) all getXbyY function calls with getaddrinfo/getnameinfo (RFC2553) calls - Add compatibility layer for target systems not supporting RFC2553 calls in rfc2553.[ch] - Add support for multiple listen sockets -- one for IPv4 and one for IPv6 (use V6ONLY) - For WIN32 platform add configuration parameter IPV6 (mutually exclusive with BINKD9X) - On WIN32 platform use Winsock2 API if IPV6 support is requested - config: node IP address literal + port supported: [<ipv6 address>]:<port>

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.590 2012/01/03 17:25:37 green Exp $
+ * $Id: Config.h,v 2.591 2012/01/03 17:26:05 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 563
+#define PATCHLEVEL 564
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
