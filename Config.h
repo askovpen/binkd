041a4adee1e7883052f4328a4537747bbff15b0b
gul <gul>
2012-01-06 11:33:33 +0000

    Auto increase patchlevel, set 1.0a-570 Format error

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.597 2012/01/06 09:44:10 gul Exp $
+ * $Id: Config.h,v 2.598 2012/01/06 11:33:33 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 569
+#define PATCHLEVEL 570
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
