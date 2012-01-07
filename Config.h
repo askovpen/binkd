a86b49482f4a8338b299feb32a2479758cede8f5
green <green>
2012-01-07 23:38:48 +0000

    Auto increase patchlevel, set 1.0a-579 Improved getnameinfo handling, retry without name resolution

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.606 2012/01/07 17:00:23 green Exp $
+ * $Id: Config.h,v 2.607 2012/01/07 23:38:48 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 578
+#define PATCHLEVEL 579
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
