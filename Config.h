c3831344f5eaaae624ac9c86fde346008d101a7c
green <green>
2012-01-07 11:54:07 +0000

    Auto increase patchlevel, set 1.0a-571 Fix MSVC6 compilation errors

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.598 2012/01/06 11:33:33 gul Exp $
+ * $Id: Config.h,v 2.599 2012/01/07 11:54:07 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 570
+#define PATCHLEVEL 571
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
