fb9b75de2da97b8778fc05ac09df390a13ef7468
green <green>
2012-01-08 02:03:50 +0000

    Auto increase patchlevel, set 1.0a-580 Implement fallback for system not exporting resolv/ns_initparse etc.

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.607 2012/01/07 23:38:48 green Exp $
+ * $Id: Config.h,v 2.608 2012/01/08 02:03:50 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 579
+#define PATCHLEVEL 580
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
