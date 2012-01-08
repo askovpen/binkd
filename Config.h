7068eff4148ca9cfb3d1f2dc12b38d3313ccd113
green <green>
2012-01-08 16:23:54 +0000

    Auto increase patchlevel, set 1.0a-585 Fixed compilation in Cygwin/MinGW

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.612 2012/01/08 14:09:06 green Exp $
+ * $Id: Config.h,v 2.613 2012/01/08 16:23:54 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 584
+#define PATCHLEVEL 585
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
