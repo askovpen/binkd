d4e00ebd11ed867ac7100705075e5d82dc5aaa00
green <green>
2012-01-22 20:40:58 +0000

    Auto increase patchlevel, set 1.0a-594 Replaces index (deprecated) with strchr

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.621 2012/01/22 13:54:15 green Exp $
+ * $Id: Config.h,v 2.622 2012/01/22 20:40:58 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 593
+#define PATCHLEVEL 594
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
