9fafcc96ef441a3eff35a12c2d75d1bd3a8b493f
green <green>
2012-01-22 01:27:23 +0000

    Auto increase patchlevel, set 1.0a-590 Implement FSP1035 support for OS/2/Watcom

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.617 2012/01/22 00:19:49 green Exp $
+ * $Id: Config.h,v 2.618 2012/01/22 01:27:23 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 589
+#define PATCHLEVEL 590
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
