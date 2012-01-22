76be5c2533c4613819d2e44ee437133a2398bb20
green <green>
2012-01-22 12:27:10 +0000

    Auto increase patchlevel, set 1.0a-592 No SRV lookup for IP addresses

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.619 2012/01/22 01:27:51 green Exp $
+ * $Id: Config.h,v 2.620 2012/01/22 12:27:10 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 591
+#define PATCHLEVEL 592
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
