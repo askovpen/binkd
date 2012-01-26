84a1916481e9f7bddcda4f5324fc5044e0f2424b
green <green>
2012-01-26 22:58:37 +0000

    Auto increase patchlevel, set 1.0a-602 Workaround for OS/2 kLIBC 0.6.4, getpeername() always returns -1

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.629 2012/01/26 18:50:10 green Exp $
+ * $Id: Config.h,v 2.630 2012/01/26 22:58:37 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 601
+#define PATCHLEVEL 602
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
