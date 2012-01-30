1e88f59d382e508d8834fd9617f311eff7fa180e
green <green>
2012-01-30 23:02:17 +0000

    Auto increase patchlevel, set 1.0a-604 Corrected FTS-1027 support, allow list of hash algos

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.631 2012/01/27 18:25:48 green Exp $
+ * $Id: Config.h,v 2.632 2012/01/30 23:02:17 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 603
+#define PATCHLEVEL 604
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
