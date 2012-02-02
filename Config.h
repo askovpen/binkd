e1bbc9304f998c117f7eb2c2582b5035df10b16f
gul <gul>
2012-02-02 08:42:47 +0000

    Auto increase patchlevel, set 1.0a-605 Fixed gcc version for emx/klibc (by Max Vasilyev)

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.632 2012/01/30 23:02:17 green Exp $
+ * $Id: Config.h,v 2.633 2012/02/02 08:42:47 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 604
+#define PATCHLEVEL 605
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
