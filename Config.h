0c9ab03410091e31476b05894b4fe05140890bce
green <green>
2012-06-20 22:41:50 +0000

    Auto increase patchlevel, set 1.0a-614 1 hour timeout for defnode resolutions

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.641 2012/06/20 21:48:14 green Exp $
+ * $Id: Config.h,v 2.642 2012/06/20 22:41:50 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 613
+#define PATCHLEVEL 614
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
