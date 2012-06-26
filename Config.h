269b8426a0d0adabab2c9955154f509f73e64358
gul <gul>
2012-06-26 09:55:48 +0000

    Auto increase patchlevel, set 1.1a-4 MD5 password is not mandatory on incoming

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.646 2012/06/26 09:44:31 gul Exp $
+ * $Id: Config.h,v 2.647 2012/06/26 09:55:48 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 3
+#define PATCHLEVEL 4
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
