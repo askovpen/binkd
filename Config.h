1fa2757766ec90653d85b6e6ea68feffbd4ca965
green <green>
2012-07-06 21:42:20 +0000

    Auto increase patchlevel, set 1.1a-7 Corrected potential double-free

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.649 2012/07/05 20:56:44 green Exp $
+ * $Id: Config.h,v 2.650 2012/07/06 21:42:20 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 6
+#define PATCHLEVEL 7
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
