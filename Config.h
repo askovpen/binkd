8c5da4a711f9d83a332d6373f6dc3494b89896d4
gul <gul>
2012-07-07 07:19:29 +0000

    Auto increase patchlevel, set 1.1a-8 X64 option for binkd-msvc build

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.651 2012/07/07 07:18:53 gul Exp $
+ * $Id: Config.h,v 2.652 2012/07/07 07:19:29 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 7
+#define PATCHLEVEL 8
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
