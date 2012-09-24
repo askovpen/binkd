24c29982d4b96aea67b5b31976c43c06f1d0aadd
gul <gul>
2012-09-24 09:09:38 +0000

    Auto increase patchlevel, set 1.1a-16 Avoid compilation warning

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.659 2012/09/24 00:26:53 gul Exp $
+ * $Id: Config.h,v 2.660 2012/09/24 09:09:38 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 15
+#define PATCHLEVEL 16
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
