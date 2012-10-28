803412769af5a2eddd6a1b7ee1203614c40eff90
green <green>
2012-10-28 21:30:17 +0000

    Auto increase patchlevel, set 1.1a-17 Corrected Segfault in config error reporting on 64bit architectures

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.660 2012/09/24 09:09:38 gul Exp $
+ * $Id: Config.h,v 2.661 2012/10/28 21:30:17 green Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 16
+#define PATCHLEVEL 17
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
