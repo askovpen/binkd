afcff49009baf35dc59f82ba7dd06a77491efadf
stas <stas>
2012-11-06 05:05:57 +0000

    Auto increase patchlevel, set 1.1a-19 more comprehensible diagnostic message

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.662 2012/11/02 11:25:36 green Exp $
+ * $Id: Config.h,v 2.663 2012/11/06 05:05:57 stas Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 18
+#define PATCHLEVEL 19
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
