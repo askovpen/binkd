6430ec2171cbf1b724e7ee7768e766763d8af52a
gul <gul>
2013-01-24 17:33:42 +0000

    Auto increase patchlevel, set 1.1a-21 Syntax error

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
  */
 
 /*
- * $Id: Config.h,v 2.664 2013/01/24 17:25:38 gul Exp $
+ * $Id: Config.h,v 2.665 2013/01/24 17:33:42 gul Exp $
  */
 
 #ifndef _Config_h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 20
+#define PATCHLEVEL 21
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
